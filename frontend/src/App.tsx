73625e08d138f867293af96f7d33f22e3a9df55a
iam-abin <iamabin1100@gmail.com>
2024-11-08 16:04:04 +0530

    feat: authenticaion pages added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import MainLayout from "./components/MainLayout";
+import Auth from "./pages/Auth";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+    },
+    {
+        path: "/auth",
+        element: <Auth />,
+    },
+    {
+        path: "/landing",
+        element: <MainLayout />,
+    },
+]);
+
+export default function ButtonUsage() {
+    return (
+        <main>
+            <RouterProvider router={appRouter}></RouterProvider>
+        </main>
+    );
 }
