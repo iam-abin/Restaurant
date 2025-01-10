89823026518263ceae441bd2a538023f79c28aa6
iam-abin <iamabin1100@gmail.com>
2025-01-10 09:16:27 +0530

    refactor: update navbar items

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -1,81 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import FadeMenu from '../list/FadeMenu';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import RightDrawer from '../drawer/RightDrawer';
 import LogoutIcon from '@mui/icons-material/Logout';
-import FlatwareIcon from '@mui/icons-material/Flatware';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import { ROLES_CONSTANTS } from '../../utils/constants';
 import { useAppSelector } from '../../redux/hooks';
 import { useConfirmationContext } from '../../context/confirmationContext';
 import { IUser } from '../../types';
+import { Drawer, IconButton } from '@mui/material';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
+import RestaurantOutlinedIcon from '@mui/icons-material/RestaurantOutlined';
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
-export interface IMenuItems {
+export interface INavItems {
+    name: string;
     to: string;
     value: string;
-}
-
-export interface IMenuItems2 {
-    name: string;
     icon: React.ReactElement;
-    to: string;
 }
 
 const NavBar = ({ currentUser, handleLogout }: { currentUser: IUser; handleLogout: () => void }) => {
     const { showConfirmation } = useConfirmationContext();
     const { myProfile } = useAppSelector((store) => store.profileReducer);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const location = useLocation();
 
     const isAdmin = currentUser && currentUser.role === ROLES_CONSTANTS.ADMIN;
     const isRestaurant = currentUser && currentUser.role === ROLES_CONSTANTS.RESTAURANT;
     const isUser = currentUser && currentUser.role === ROLES_CONSTANTS.USER;
 
-    const menuItemsAdmin: (IMenuItems | boolean)[] = [
-        isAdmin && { to: '/admin/users', value: 'Users' },
-        isAdmin && { to: '/admin/restaurants', value: 'Restaurants' },
-    ].filter(Boolean); // Ensures no false or undefined values
+    const navItems: INavItems[] = [
+        // Common
+        { name: 'Home', to: '/', value: 'Home', icon: <HomeOutlinedIcon /> },
+        // Admin
+        isAdmin && { name: 'Users', to: '/admin/users', value: 'Users', icon: <PeopleOutlineOutlinedIcon /> },
+        isAdmin && {
+            name: 'Restaurants',
+            to: '/admin/restaurants',
+            value: 'Restaurants',
+            icon: <RestaurantOutlinedIcon />,
+        },
 
-    const menuItems: (Partial<IMenuItems2> | boolean)[] = [
-        currentUser &&
-            (isUser || isRestaurant) && {
-                name: 'profile',
-                icon: <Avatar src="/broken-image.jpg" />,
-                to: '/profile',
-            },
-        currentUser &&
-            isUser && {
-                name: 'Orders',
-                icon: <FlatwareIcon />,
-                to: '/order/status',
-            },
-        currentUser && isUser && { name: 'Cart', icon: <ShoppingCartIcon />, to: '/cart' },
-        currentUser &&
-            (isUser || isRestaurant) && {
-                name: 'Home',
-                icon: <ShoppingCartIcon />,
-                to: '/',
-            },
-        currentUser &&
-            isRestaurant && {
-                name: 'Restaurant',
-                icon: <MenuBookIcon />,
-                to: '/restaurant',
-            },
-        currentUser &&
-            isRestaurant && {
-                name: 'Menu',
-                icon: <MenuBookIcon />,
-                to: '/restaurant/menu',
-            },
-        currentUser &&
-            isRestaurant && {
-                name: 'Restaurant orders',
-                icon: <MenuBookIcon />,
-                to: '/restaurant/orders',
-            },
-        { name: 'logout', icon: <LogoutIcon /> },
-    ];
+        // User
+        isUser && { name: 'Profile', to: '/profile', value: 'Profile', icon: <AccountCircleOutlinedIcon /> },
+        isUser && { name: 'Orders', to: '/orders', value: 'Orders', icon: <LocalMallOutlinedIcon /> },
+        // Restaurant
+        isRestaurant && {
+            name: 'Restaurant',
+            to: '/restaurant/details',
+            value: 'Restaurant',
+            icon: <RestaurantOutlinedIcon />,
+        },
+        isRestaurant && {
+            name: 'Menu',
+            to: '/restaurant/menu',
+            value: 'Menu',
+            icon: <MenuBookOutlinedIcon />,
+        },
+        isRestaurant && {
+            name: 'Restaurant Orders',
+            to: '/restaurant/orders',
+            value: 'Restaurant Orders',
+            icon: <HomeOutlinedIcon />,
+        },
+    ].filter(Boolean) as INavItems[]; // Cast to INavItems[] after filtering out false values
+
     const handleLogoutButton = () => {
         showConfirmation({
             title: 'Do you want to logout',
@@ -85,40 +78,84 @@ const NavBar = ({ currentUser, handleLogout }: { currentUser: IUser; handleLogou
             okayText: 'Yes logout',
         });
     };
+
+    // Active link
+    const isActiveLink = (path: string) =>
+        location.pathname === path ? 'font-bold border-b-2 pb-2 border-orange-600' : 'text-gray-600';
+
     return (
         <div className="md:max-w-7xl px-3 md:mx-auto bg-white shadow-lg">
             <div className="flex items-center justify-between h-14">
                 <Link to="/">
                     <h1 className="font-bold md:font-extrabold text-2xl">Restaurant App</h1>
                 </Link>
+                {/* Desktop Menu */}
                 <div className="hidden md:flex gap-10 items-center ">
-                    {currentUser && isUser && (
-                        <>
-                            <Link to={'/'}>Home</Link>
-                            <Link to={'/profile'}>Profile</Link>
-                            <Link to={'/orders'}>Orders</Link>
-                        </>
-                    )}
-
-                    {currentUser && isRestaurant && (
-                        <>
-                            <Link to={'/restaurant'}>Home</Link>
-                            <Link to={'/restaurant/menu'}>Menu</Link>
-                            <Link to={'/restaurant/details'}>Details</Link>
-                            <Link to={'/restaurant/orders'}>Orders</Link>
-                        </>
-                    )}
+                    {navItems.map((item) => (
+                        <Link
+                            key={item.to}
+                            to={item.to}
+                            className={`flex items-center gap-2 ${isActiveLink(item.to)}`}
+                        >
+                            <span>{item.value}</span>
+                        </Link>
+                    ))}
 
                     <Avatar src={`${myProfile?.imageUrl ? myProfile?.imageUrl : '/broken-image.jpg'}`} />
                     <LogoutIcon onClick={handleLogoutButton} style={{ cursor: 'pointer' }} />
                 </div>
-                {isAdmin && <FadeMenu menuItems={menuItemsAdmin} />}
-                {/* Mobile responsiveness */}
-                {!isAdmin && (
-                    <div className="md:hidden">
-                        <RightDrawer onClickFn={handleLogoutButton} menuItems={menuItems} />
+                {/* Mobile Menu */}
+                <div className="md:hidden">
+                    <IconButton onClick={() => setIsDrawerOpen(true)}>
+                        <MenuIcon />
+                    </IconButton>
+                </div>
+                <Drawer anchor="right" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+                    <div className="flex flex-col gap-10 p-4 h-screen min-w-[250px]">
+                        {/* Top */}
+                        <div className="flex  justify-between items-center">
+                            <h2 className="font-bold text-xl">Select</h2>
+                            <IconButton onClick={() => setIsDrawerOpen(false)}>
+                                <CloseIcon />
+                            </IconButton>
+                        </div>
+                        <div className='flex flex-col justify-between h-full'>
+                            {/* Middle */}
+                            <div className="flex flex-col justify-between gap-8 mt-4">
+                                {navItems.map((item) => (
+                                    <Link
+                                        key={item.to}
+                                        to={item.to}
+                                        className={`flex items-center gap-2 text-gray-800 ${isActiveLink(item.to)}`}
+                                        onClick={() => setIsDrawerOpen(false)}
+                                    >
+                                        {item.icon && item.icon}
+                                        <span>{item.value}</span>
+                                    </Link>
+                                ))}
+                            </div>
+
+                            {/* Bottom */}
+                            <div className="flex justify-between pr-5">
+                                <div className="flex items-center gap-2 mt-4">
+                                    <Avatar
+                                        src={`${myProfile?.imageUrl ? myProfile?.imageUrl : '/broken-image.jpg'}`}
+                                    />
+                                    <span>{(myProfile?.userId as IUser).name || 'Profile'}</span>
+                                </div>
+                                <div
+                                    className="flex items-center gap-2 mt-4 cursor-pointer text-red-500"
+                                    onClick={handleLogoutButton}
+                                >
+                                    {/* <Tooltip title='asdf'> */}
+
+                                    <LogoutIcon />
+                                    {/* </Tooltip> */}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                )}
+                </Drawer>
             </div>
         </div>
     );
